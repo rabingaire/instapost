7f48e6d7d033d2aafd775e5366774903d4fd93ea
Rabin Gaire <rabingaire20@gmail.com>
2016-08-29 01:02:35 +0545

    Login Design Added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,10 @@ class PostsController < ApplicationController
     @posts = Post.all.order('created_at DESC')
   end
 
+  def show
+  	@post = Post.find(params[:id])
+	end
+
   def new
     @post = Post.new
   end
@@ -45,7 +49,7 @@ class PostsController < ApplicationController
 	def is_owner?
   	redirect_to root_path if Post.find(params[:id]).user != current_user
 	end
-	
+
 	def post_params
   	params.require(:post).permit(:user_id, :photo, :description)
 	end
