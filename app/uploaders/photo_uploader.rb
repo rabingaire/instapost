1577833e2e7b56595d31ec85f39b0f609289547a
Rabin Gaire <rabingaire20@gmail.com>
2016-09-03 22:23:56 +0545

    Added Cloudinary

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 class PhotoUploader < CarrierWave::Uploader::Base
-
+  include Cloudinary::CarrierWave
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -47,6 +47,5 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-  include Cloudinary::CarrierWave
-  
+
 end
